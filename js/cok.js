05d0f96160c2e41b7ed1d1e43774f212e04d754c
seastj <dev.seastj@gmail.com>
2025-07-07 18:11:19 +0900

    [docs] cok.js 진행중

---
+++ js/cok.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function () {
+  const cokDataArr = [
+    {
+      link: "#",
+      img: "images/cok1.png",
+      alt: "스포츠 윤리 센터",
+      label: { 공모전, 교육 },
+      option:
+        "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
+      date: "D-28",
+    },
+  ];
+
+  // html 태그 만들기
+  let html = ``;
+
+  // 실제 데이터 갯수 만큼 이미지 만들기
+  html = ``;
+});
