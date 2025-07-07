c43ad6b717781815af48105c764a9437af0b799a
seastj <dev.seastj@gmail.com>
2025-07-08 00:20:35 +0900

    [docs] cok.js 진행중

--- js/cok.js
+++ js/cok.js
@@ -4,7 +4,38 @@ window.addEventListener("load", function () {
       link: "#",
       img: "images/cok1.png",
       alt: "스포츠 윤리 센터",
-      label: { 공모전, 교육 },
+      label: ["공모전", "교육", "인턴십", "대외활동"],
+      city: "서울 종로구",
+      option:
+        "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
+      date: "D-28",
+    },
+    {
+      link: "#",
+      img: "images/cok2.png",
+      alt: "스포츠 윤리 센터",
+      label: ["공모전", "교육", "인턴십", "대외활동"],
+      city: "서울 종로구",
+      option:
+        "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
+      date: "D-28",
+    },
+    {
+      link: "#",
+      img: "images/cok3.png",
+      alt: "스포츠 윤리 센터",
+      label: ["공모전", "교육", "인턴십", "대외활동"],
+      city: "서울 종로구",
+      option:
+        "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
+      date: "D-28",
+    },
+    {
+      link: "#",
+      img: "images/cok4.png",
+      alt: "스포츠 윤리 센터",
+      label: ["공모전", "교육", "인턴십", "대외활동"],
+      city: "서울 종로구",
       option:
         "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
       date: "D-28",
@@ -14,6 +45,31 @@ window.addEventListener("load", function () {
   // html 태그 만들기
   let html = ``;
 
-  // 실제 데이터 갯수 만큼 이미지 만들기
-  html = ``;
+  for (i = 0; i < cokDataArr.length; i++) {
+    const item = cokDataArr[i];
+
+    // city 가 있으면 생성 없을시 삭제
+    let cityHtml = ``;
+
+    if (item.city) {
+      cityHtml = ` <div class="cok_info_city">${item.city}</div>`;
+    }
+
+    html += `<a href="${item.link}" class="cok_list">
+                <div class="cok_list_image">
+                  <img src="${item.img}" alt="${item.alt}" />
+                </div>
+                <div class="cok_list_info">
+                  <div class="cok_label">
+                    <div class="cok_info_cup">${item.label}</div>
+                    ${cityHtml}</div>
+                  <div class="cok_info_option">
+                    ${item.option}
+                  </div>
+                  <div class="cok_info_date">${item.date}</div>
+                </div>
+              </a>`;
+  }
+
+  document.querySelector(".cok_content").innerHTML = html;
 });
