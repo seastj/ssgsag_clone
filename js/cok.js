ec7ee9ea2965ddc28d8cd8810b578cfbf98c9b76
seastj <dev.seastj@gmail.com>
2025-07-08 17:19:12 +0900

    [docs] cok.js 배열

--- js/cok.js
+++ js/cok.js
@@ -4,8 +4,8 @@ window.addEventListener("load", function () {
       link: "#",
       img: "images/cok1.png",
       alt: "스포츠 윤리 센터",
-      label: ["공모전", "교육", "인턴십", "대외활동"],
-      city: "서울 종로구",
+      label: [{ text: "공모전", style: "cok_label_blue" }],
+      city: "",
       option:
         "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
       date: "D-28",
@@ -14,8 +14,8 @@ window.addEventListener("load", function () {
       link: "#",
       img: "images/cok2.png",
       alt: "스포츠 윤리 센터",
-      label: ["공모전", "교육", "인턴십", "대외활동"],
-      city: "서울 종로구",
+      label: [{ text: "대외활동", style: "cok_label_blue" }],
+      city: "",
       option:
         "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
       date: "D-28",
@@ -24,8 +24,8 @@ window.addEventListener("load", function () {
       link: "#",
       img: "images/cok3.png",
       alt: "스포츠 윤리 센터",
-      label: ["공모전", "교육", "인턴십", "대외활동"],
-      city: "서울 종로구",
+      label: [{ text: "", style: "cok_label_blue" }],
+      city: "",
       option:
         "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
       date: "D-28",
@@ -34,7 +34,7 @@ window.addEventListener("load", function () {
       link: "#",
       img: "images/cok4.png",
       alt: "스포츠 윤리 센터",
-      label: ["공모전", "교육", "인턴십", "대외활동"],
+      label: [{ text: "", style: "cok_label_blue" }],
       city: "서울 종로구",
       option:
         "[스포츠윤리센터] 스포츠윤리센터 성희롱·성폭력 예방 숏폼 영상공모전",
@@ -45,7 +45,7 @@ window.addEventListener("load", function () {
   // html 태그 만들기
   let html = ``;
 
-  for (i = 0; i < cokDataArr.length; i++) {
+  for (let i = 0; i < cokDataArr.length; i++) {
     const item = cokDataArr[i];
 
     // city 가 있으면 생성 없을시 삭제
@@ -55,13 +55,20 @@ window.addEventListener("load", function () {
       cityHtml = ` <div class="cok_info_city">${item.city}</div>`;
     }
 
+    let labHtml = ``;
+    if (item.label) {
+      labHtml = `<div class="${item.style}">${item.text}</div>`;
+    }
+
     html += `<a href="${item.link}" class="cok_list">
                 <div class="cok_list_image">
                   <img src="${item.img}" alt="${item.alt}" />
                 </div>
                 <div class="cok_list_info">
                   <div class="cok_label">
-                    <div class="cok_info_cup">${item.label}</div>
+${labHtml}</div>
+                    
+                  
                     ${cityHtml}</div>
                   <div class="cok_info_option">
                     ${item.option}
